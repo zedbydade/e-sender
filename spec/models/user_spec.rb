01b1d3aecfa48cf3ee4ae124a770a22b224a3eda
archierb <franciscoleite.dev@protonmail.com>
2023-01-01 20:37:22 -0300

    Tests files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#priority_user?' do
+    let(:user) { create(:user) }
+
+    context 'balance more than User::PRIORITY_USER_MINIMUM_BALANCE' do
+      it 'return true' do
+        account = create(:account, balance: 200_000, user: user)
+
+        expect(user.priority_user?).to eq(true)
+      end
+    end
+
+    context 'balance less than User::PRIORITY_USER_MINIMUM_BALANCE' do
+      it 'return false' do
+        account = create(:account, balance: 50_000, user: user)
+
+        expect(user.priority_user?).to eq(false)
+      end
+    end
+  end
 end
